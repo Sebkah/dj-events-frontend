228ddb4e5704dd69c26c80283bbcc8e095e5bd8d
Sebkah <88620476+Sebkah@users.noreply.github.com>
2021-11-26 17:17:11 +0100

    .

---
+++ pages/events/[slug].js
@@ -0,0 +1,8 @@
+import Layout from '../../component/Layout';
+export default function EventPage() {
+  return (
+    <Layout>
+      <h1>My Event</h1>
+    </Layout>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,8 @@
+import Layout from '../../component/Layout';
+export default function EventsPage() {
+  return (
+    <Layout>
+      <h1>My events</h1>
+    </Layout>
+  );
+}
